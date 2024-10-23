de9689b24a15946c8e4524db602f75d98ddc1eb0
SujalR7 <sujalrandive7@gmail.com>
2024-10-23 08:30:40 +0530

    Update ToDoList.js

--- ToDoList.js
+++ ToDoList.js
@@ -1,9 +1,58 @@
-let btn=document.querySelector("button");
+let btn=document.querySelector(".button");
 let input=document.querySelector("input");
 let ul=document.querySelector(".section");
 let ul1=document.querySelector(".section1");
+let p=document.querySelector(".time");
+let start=document.querySelector("#start");
+let lap=document.querySelector("#lap");
+let reset=document.querySelector("#reset");
+let para=document.querySelector("#counter");
+let content=document.querySelector(".content");
+let count=0,count1=0;
+let click=1;
+const showTime=()=>{
+    let date=new Date();
+    p.innerHTML=date.toLocaleTimeString();
+}
+let IntervalId=setInterval(showTime,1000);
+setTimeout(()=>{clearInterval(IntervalId)},5000);
+let intervalid,min,second,lapid=1;
+const start_timer=()=>{
+    if(click==1)
+    {
+     start.innerHTML="||";
+    intervalid=setInterval(()=>{count=count+1;min=count/60;
+        min=Math.trunc(min);
+        second=count%60;
+        para.innerHTML=min+":"+second;
+    },1000);
+    click=0;
+    }
+    else if(click==0)
+    {
+        start.innerHTML="&#9654;";
+        clearInterval(intervalid);
+        click=1;
+    }
+}
+const reset_timer=()=>{
+    count=0;
+    para.innerHTML=count;
+    clearInterval(intervalid);
+    start.innerHTML="&#9654;";
+    content.innerHTML="";
+}
+const showdetails=()=>{
+let li=document.createElement("li");
+li.innerText=min+":"+second;
+content.append(li);
+}
+start.addEventListener("click",start_timer);
+reset.addEventListener("click",reset_timer);
+lap.addEventListener("click",showdetails);
 ul1.style.display="none";
 let visit=0,visit1=0;
+showTime();
 const display=(list)=>{
     let div=document.createElement("div");
     div.classList.add("task");
